8aae13c1cf7390bdc78c370f308af767dbfb3560
WeikaiLi-Kevin <weikaili305@gmail.com>
2017-01-27 15:19:03 -0500

    Added 3 activities for Lab 2

--- app/src/main/java/com/weikaili/androidlabs/LoginActivity.java
+++ app/src/main/java/com/weikaili/androidlabs/LoginActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
